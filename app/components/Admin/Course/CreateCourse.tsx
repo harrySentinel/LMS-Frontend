7860b27d6d9e5faf5b171edd9f3d4f1324a0b142
Variant25 <ayushp041@gmail.com>
2024-09-14 17:04:39 +0530

    course preview

--- app/components/Admin/Course/CreateCourse.tsx
+++ app/components/Admin/Course/CreateCourse.tsx
@@ -10,7 +10,7 @@ import CoursePreview from "./CoursePreview";
 type Props = {};
 
 const CreateCourse = (props: Props) => {
-  const [active, setActive] = useState(0);
+  const [active, setActive] = useState(3);
   const [courseInfo, setCourseInfo] = useState({
     name: "",
     description: "",
